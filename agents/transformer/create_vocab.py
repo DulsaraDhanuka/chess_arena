de2a04ecbba44b312d88a652a62f71ff42a0510b
Dulsara Dhanuka <dulsara.dhanuka@local.com>
2024-01-27 14:56:14 +0530

    Implemented <s:WHITE>, <s:BLACK>, <s:DRAW> tokens

---
+++ agents/transformer/create_vocab.py
@@ -0,0 +1,37 @@
+vocab = {}
+index = 0
+
+itof = {0: 'a', 1: 'b', 2: 'c', 3: 'd', 4: 'e', 5: 'f', 6: 'g', 7: 'h'}
+itor = {0: '1', 1: '2', 2: '3', 3: '4', 4: '5', 5: '6', 6: '7', 7: '8'}
+
+def insert_vocab(move):
+    global index, vocab
+    vocab[move] = index
+    vocab[index] = move
+    index += 1
+
+insert_vocab("<s:WHITE>")
+insert_vocab("<s:BLACK>")
+insert_vocab("<s:DRAW>")
+insert_vocab("</s>")
+
+for i in range(8):
+    for j in range(8):
+        for k in range(8):
+            for l in range(8):
+                insert_vocab(f"{itof[i]}{itor[j]}{itof[k]}{itor[l]}")
+
+for i in range(8):
+    for piece in ['b', 'n', 'q', 'r']:
+        if i != 0:
+            insert_vocab(itof[i] + '7' + itof[i-1] + '8' + piece)
+        if i != 7:
+            insert_vocab(itof[i] + '7' + itof[i+1] + '8' + piece)
+        insert_vocab(itof[i] + '7' + itof[i] + '8' + piece)
+
+print(vocab)
+
+import pickle
+with open("vocab.pickle", "wb") as f:
+    pickle.dump(vocab, f)
+
--- agents/transformer/prepare_data.py
+++ agents/transformer/prepare_data.py
@@ -9,6 +9,9 @@ data_path = os.path.join(dir_path, "data_pgn")
 
 pgn_files = [f for f in os.listdir(data_path) if os.path.isfile(os.path.join(data_path, f)) and os.path.splitext(f)[1] == ".pgn"]
 enc = Encoding()
+white_games = 0
+black_games = 0
+draw_games = 0
 tokens = []
 for pgn_name in pgn_files:
     with open(os.path.join(data_path, pgn_name), 'r', encoding='utf-8') as pgn:
@@ -16,14 +19,28 @@ for pgn_name in pgn_files:
             try:
                 game = chess.pgn.read_game(pgn)
                 if game is None: break
-                tokens.append(enc.encode("<s>"))
+                game_status = "DRAW"
+                if game.headers["Result"].split("-")[0] == "1":
+                    game_status = "WHITE"
+                    white_games += 1
+                elif game.headers["Result"].split("-")[1] == "1":
+                    game_status = "BLACK"
+                    black_games += 1
+                else:
+                    draw_games += 1
+                tokens.append(enc.encode(f"<s:{game_status}>"))
                 for move in game.mainline_moves():
                     tokens.append(enc.encode(move.uci()))
                 tokens.append(enc.encode("</s>"))
             except Exception as e:
                 continue
 n_vocab = enc.n_vocab
+print()
+print("White games: ", white_games)
+print("Black games: ", black_games)
+print("Draw games: ", draw_games)
 print(f"Training data loaded - {len(tokens)} tokens")
 
 with open("data.npy", "wb") as f:
     np.save(f, np.array(tokens, dtype=np.int32))
+
